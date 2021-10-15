662f3400cd2f48f42d8faf56b21d6a747f572796
Viktorherald <viktorng@protonmail.com>
2021-10-16 02:15:44 +0800

    slash command try

--- index.js
+++ index.js
@@ -7,4 +7,19 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name is: ${interaction.guild.name}\nTotal Members: ${interaction.guild.memberCount}` + 
+        `\nVerification level: ${interaction.guild.verificationLevel}`);
+	} else if (commandName === 'user') {
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	}
+});
+
 client.login(token);
